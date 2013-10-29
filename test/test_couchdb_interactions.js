3353052905e9f75d4e5666d8d24ec20de0eca1e0
James E. Marca <jmarca@translab.its.uci.edu>
2013-10-28 17:48:04 -0700

    working to do something useful, suck on couchdb junk
    modified:   .gitignore
    deleted:    config.json
    modified:   lib/config_okay.js
    modified:   lib/couchdb_interactions.js
    modified:   package.json
    renamed:    test/test_filter_grids.js -> test/test_couchdb_interactions.js

--- test/test_couchdb_interactions.js
+++ test/test_couchdb_interactions.js
@@ -4,6 +4,7 @@ var should = require('should')
 
 var async = require('async')
 var _ = require('lodash')
+
 var cdb_interactions = require('../lib/couchdb_interactions')
 var filter_grids = cdb_interactions.filter_out_done
 var mark_done = cdb_interactions.mark_done
@@ -11,21 +12,67 @@ var in_process = cdb_interactions.mark_in_process
 
 var get_detector_routes = require('../lib/query_postgres').get_detector_route_nums
 var fs = require('fs')
+var superagent=require('superagent')
 
 var config_okay = require('../lib/config_okay')
 
-var task,options;
-before(config_okay('config.json',function(err,c){
-           options ={'couchdb':c.couchdb}
+var task,options,created_locally=false;
+before(function(done){
+    config_okay('test.config.json',function(err,c){
+        options ={'couchdb':c.couchdb}
+        options.couchdb.db.should.eql('testdb')
+        options.couchdb.statedb.should.eql('teststatedb')
 
-           // dummy up a done grid and a not done grid in a test db
-           task = {'options':options};
-           task.cell_id= '178_92'
-           task.year   = 2007
+        // dummy up a done grid and a not done grid in a test db
+        task = {'options':options};
+        task.cell_id= '178_92'
+        task.year   = 2007
+        async.each([options.couchdb.db,options.couchdb.statedb]
+                  ,function(db,cb){
+                       var cdb =
+                           [task.options.couchdb.url
+                           ,db].join('/')
+                       console.log(cdb)
+                       superagent.put(cdb)
+                       .type('json')
+                       .auth(options.couchdb.auth.username
+                            ,options.couchdb.auth.password)
+                       .end(function(err,result){
 
-       }))
+                           if(result.error===undefined){
+                               created_locally=true
+                           }else{
+                               console.log(result)
+                           }
+                           cb()
+                       })
+                   }
+                  ,done
+                  );
+        return null
+    })
+})
+after(function(done){
+    if(created_locally){
+        async.each([options.couchdb.db,options.couchdb.statedb]
+                  ,function(db,cb){
+                       var cdb =
+                           [task.options.couchdb.url
+                           ,db].join('/')
+                       console.log(cdb)
+                       superagent.del(cdb)
+                       .type('json')
+                       .auth(options.couchdb.auth.username
+                            ,options.couchdb.auth.password)
+                       .end(cb)
+                   })
+            return null
+    }else{
+        return done()
+    }
+})
 
-describe('can mark as in process',function(){
+describe('can mark as inprocess',function(){
     before(function(done){
         in_process(task,function(err,state){
                should.not.exist(err)
