fd67901ac497006048bf17dc8582aef5b5107108
James E. Marca <james@activimetrics.com>
2014-12-03 22:30:12 -0800

    added index.js for exporting

---
+++ index.js
@@ -0,0 +1,17 @@
+var cdbi = require('./lib/couchdb_interactions.js')
+var mmap = require('./lib/make_map.js')
+var reduce = require('./lib/reduce.js')
+var flatten = require('./lib/flatten.js')
+
+exports.filter_out_done= cdbi.filter_out_done
+exports.mark_done= cdbi.mark_done
+exports.mark_in_process= cdbi.in_process
+exports.get_hpms_fractions= cdbi.get_hpms_fractions
+exports.get_detector_fractions= cdbi.get_detector_fractions
+exports.put_results_doc= cdbi.put_results_doc
+
+exports.flatten_records = flatten.flatten_records
+
+exports.all_tasks=mmap.all_tasks
+
+exports.post_process_couch_query=reduce.post_process_couch_query
