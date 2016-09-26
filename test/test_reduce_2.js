79c761d3b989062ac9c6b9d307fdc60e68eea15e
James E. Marca <james@activimetrics.com>
2016-09-26 11:34:35 -0700

    test for reduce (post process couchdb) passes
    working for both grid and timestamp case

--- test/test_reduce_2.js
+++ test/test_reduce_2.js
@@ -19,7 +19,9 @@ var utils = require('./utils')
 var superagent = require('superagent')
 var cdb_interactions = require('../lib/couchdb_interactions')
 var get_hpms_fractions = cdb_interactions.get_hpms_fractions
+var get_hpms_fractions_one_hour = cdb_interactions.get_hpms_fractions_one_hour
 var get_detector_fractions = cdb_interactions.get_detector_fractions
+var get_detector_fractions_one_hour = cdb_interactions.get_detector_fractions_one_hour
 
 var path = require('path')
 var rootdir = path.normalize(__dirname)
@@ -69,5 +71,37 @@ describe('post process couch query',function(){
             return done()
         })
         return null;
-    })}
-)
+    })
+    it('should compute scale of not 1,1,1 for hpms data',function(done){
+        var task ={'options':options
+                   ,'year':2008
+                   ,'ts':"2008-01-11 15:00"
+                  }
+        queue(1)
+        .defer(get_detector_fractions_one_hour,task)
+        .defer(get_hpms_fractions_one_hour,task)
+        .defer(reduce.post_process_couch_query_one_hour,task)
+        .await(function(e,t1,t2,t3){
+            should.not.exist(e)
+            should.exist(task)
+            task.should.have.property('scale')
+
+            task.scale.should.have.keys('100_223','178_97','134_163',
+                                        '132_164','189_72');
+
+            ["100_223","178_97"].forEach(function(cellid){
+                task.scale[cellid].n.should.not.eql(1)
+                task.scale[cellid].hh.should.not.eql(1)
+                task.scale[cellid].nhh.should.not.eql(1)
+            });
+
+            ['134_163','132_164','189_72'].forEach(function(cellid){
+                task.scale[cellid].n.should.eql(1)
+                task.scale[cellid].hh.should.eql(1)
+                task.scale[cellid].nhh.should.eql(1)
+            })
+            return done()
+        })
+        return null;
+    })
+})
